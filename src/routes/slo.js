16c728ff559107858000436741989ac306c446ad
Alex Zeltser <alexander.zeltser@sap.com>
2020-01-20 10:33:19 +0200

    added support for relayState

--- src/routes/slo.js
+++ src/routes/slo.js
@@ -8,16 +8,16 @@ import sessions from '../services/sessions';
 
 
 router.post("/:spName/:domain/:apiKey/slo", function (req, res) {
-
     var idp = config.getIdp(req.params.domain, req.params.apiKey);
     var sp = config.getSp(req.params.spName);
- 
     var options = {
         request_body: req.body,
         audience: config.getEntityId(req.params.spName),
         ignore_signature: false,
-        allow_unencrypted_assertion: true
+        allow_unencrypted_assertion: true,
+        destination: req.body.RelayState
     };
+
 
     sp.post_assert(idp, options, function (err, saml_response) {
         logger.log(saml_response, "post");
@@ -31,7 +31,7 @@ router.post("/:spName/:domain/:apiKey/slo", function (req, res) {
 
 
 router.get("/:spName/:domain/:apiKey/slo", function (req, res) {
-
+    return res.send("this is the get function");
     var idp = config.getIdp(req.params.domain, req.params.apiKey);
     var sp = config.getSp(req.params.spName);
 
@@ -39,9 +39,9 @@ router.get("/:spName/:domain/:apiKey/slo", function (req, res) {
         request_body: req.query,
         audience: config.getEntityId(req.params.spName),
         ignore_signature: false,
-        allow_unencrypted_assertion: true
+        allow_unencrypted_assertion: true,
+        destination: req.params.RelayState
     };
-
     sp.redirect_assert(idp, options, function (err, saml_response) {
         logger.log(saml_response, "get");
         return slo(saml_response, sp, idp, options, res, err);
@@ -53,9 +53,14 @@ router.get("/:spName/:domain/:apiKey/slo", function (req, res) {
 
 
 function slo(saml_response, sp, idp, o, res, err) {
+    if (o.destination != null) {
+        console.log("RelayState: " + o.destination);
+        // res.redirect(o.destination);
+        return  res.send("this is the post function the relayState: " + o.destination);
+    }
     if (saml_response && saml_response.type == 'logout_request') {
 
-        var options = {in_response_to: saml_response.response_header.id,  sign_get_request: true};
+        var options = {in_response_to: saml_response.response_header.id, sign_get_request: true};
         sp.create_logout_response_url(idp, options, function (url_err, logout_url) {
             if (url_err != null)
                 return res.send(url_err);
@@ -67,5 +72,12 @@ function slo(saml_response, sp, idp, o, res, err) {
         res.json(saml_response);
     }
 }
+
+
+// function redirectToRelayState(relayState,res) {
+//     // console.log("RelayState: " + relayState);
+//     // return res.send("this is the post function the relayState: " + relayState);
+//     //return res.redirect(relayState);
+// }
 
 export default router;
